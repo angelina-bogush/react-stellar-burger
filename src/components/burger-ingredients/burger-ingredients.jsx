5da5d5d004bc944ca57f6a736077cba60138040c
Angelina Bogush <angelinabogush@yandex.ru>
2023-08-26 19:17:25 +0300

    fix Burger Ingredients scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -5,6 +5,10 @@ import BurgerSection from './burger-section/burger-section';
 import BurgerCard from './burger-card/burger-card';
 import blueImgPath from '../../images/blue.png';
 import pinkImgPath from '../../images/pink.png';
+import greenSaucePath from '../../images/sauce-green.png';
+import darkSaucePath from '../../images/sauce-dark.png';
+import pinkSaucePath from '../../images/sauce-pink.png';
+import yellowSaucePath from '../../images/sauce-yellow.png';
 function BurgerIngredients() {
   return (
     <div className={`${styles.container} pt-5`}>
@@ -27,13 +31,25 @@ function BurgerIngredients() {
 
       <BurgerSection title={"Соусы"}>
         <BurgerCard
-          img={blueImgPath}
+          img={darkSaucePath}
           alt={"Соус Spicy-X"}
           price={30}
           description={"Соус Spicy-X"}
         />
         <BurgerCard
-          img={pinkImgPath}
+          img={greenSaucePath}
+          alt={"Соус фирменный Space Sauce"}
+          price={30}
+          description={"Соус фирменный Space Sauce"}
+        />
+        <BurgerCard
+          img={yellowSaucePath}
+          alt={"Соус фирменный Space Sauce"}
+          price={30}
+          description={"Соус фирменный Space Sauce"}
+        />
+        <BurgerCard
+          img={pinkSaucePath}
           alt={"Соус фирменный Space Sauce"}
           price={30}
           description={"Соус фирменный Space Sauce"}
